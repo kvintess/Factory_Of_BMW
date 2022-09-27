8676682d0745688663cced99ef543ba81842ceb4
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 13:22:35 +0300

    2 научилсяудалять аттрибуты

--- MyOwnCar.py
+++ MyOwnCar.py
@@ -14,3 +14,7 @@ while my_car.current_velocity < 100:
     my_car.current_velocity += 1
 #разгоню её до 100
 print(my_car.current_velocity, 'км в час после разгона')
+
+my_car.error_attribute = 'я хочу удалить этот аттрибут'
+del my_car.error_attribute
+print(my_car.error_attribute)
