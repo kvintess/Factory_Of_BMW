b48997597d7c8439861377ff32519b27079aee71
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 13:18:26 +0300

    1 Побаловался с машиной, разогнал её, испачкал

--- CharacteristicsOfCars.py
+++ CharacteristicsOfCars.py
@@ -4,8 +4,8 @@ class Bmw:
         self.color = 'черный'
         self.price = '3000000 руб.'
         self.engine_rpm = 0
-        self.current_velocity = '0 км/ч'
-        self.max_velocity = '300 км/ч'
+        self.current_velocity = 0
+        self.max_velocity = 300
 
     def start(self):
         print('мотор запущен')
@@ -14,4 +14,4 @@ class Bmw:
     def go(self):
         print('поехали!')
         self.engine_rpm = 2000
-        self.current_velocity = '20 км/ч'
+        self.current_velocity = 20
---
+++ MyOwnCar.py
@@ -0,0 +1,16 @@
+from FactoryOfCars import stock
+
+my_car=stock[299]
+#я купил 300-ю машину у этого завода. Единственная белая машина из партии
+my_car.go()
+#я на ней поехал
+print(my_car.current_velocity, 'км в час после старта')
+#текущая скорость
+my_car.is_clean = 'машина стала грязной'
+#создал аттрибут, который говорит,чистая машина или нет
+print(my_car.is_clean)
+#я проехался на ней и она стала грязной
+while my_car.current_velocity < 100:
+    my_car.current_velocity += 1
+#разгоню её до 100
+print(my_car.current_velocity, 'км в час после разгона')
